c9a6695651a3260eaecf9f079ecb2bfa88db20e7
DELL <DELL@DESKTOP-A4JBNON>
2025-03-16 15:46:38 -0600

    Agregar archivos de proyecto.

---
+++ Practica20250311.Dbfirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica20250311.Dbfirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
